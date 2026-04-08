7de25fe510a29217f07a89b6f014ad8f1c1db6a5
zotel1 <zotelsigel@gmail.com>
2026-04-08 20:21:23 -0300

    add, Ejercicios de if-else

---
+++ src/EjercicioIf.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EjercicioIf {
+
+    public static void main(String[] args) {
+        int edad;
+        String nombre;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre");
+
+        nombre =  teclado.next();
+
+        System.out.println("Ingrese su edad");
+        edad = teclado.nextInt();
+
+
+        if (edad >= 18) {
+            System.out.println("Felicidades " + nombre + " sos mayor de edad");
+        } else
+            System.out.println(nombre + "No podes acceder :/... " +  " sos menor de edad");
+    }
+}
