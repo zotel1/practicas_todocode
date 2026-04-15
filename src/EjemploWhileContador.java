d63416ffd6587111b6a5e7487783dc346758baae
zotel1 <zotelsigel@gmail.com>
2026-04-14 22:29:37 -0300

    implementacion de ejecucion de bucle por contador

---
+++ src/EjemploWhileContador.java
@@ -0,0 +1,13 @@
+public class EjemploWhileContador {
+
+    public static void main(String[] args) {
+
+    // Ejemplo bucle controlado por contador
+        int contador = 0;
+
+        while (contador < 10) {
+            System.out.println("Estoy en la vuekta numero: " + contador);
+            contador++;
+        }
+    }
+}
