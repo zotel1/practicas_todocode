f9041cfdfc2986796d1247e3578f2fd36b26f010
zotel1 <zotelsigel@gmail.com>
2026-04-17 00:55:19 -0300

    ejemplo estructura repetitiva

---
+++ src/EjemploForSecuencia.java
@@ -0,0 +1,13 @@
+public class EjemploForSecuencia {
+
+    // Realizar un programa que muestre por pantalla los numeros del 200 a 250 saltando de 2 en 2
+    public static void main(String[] args) {
+
+        int cont = 200;
+        while (cont <= 250) {
+            System.out.println(cont );
+            cont += 2;
+        }
+
+    }
+}
