e539d88c256d9c295f9e76c5d02ea911611fbcbc
zotel1 <zotelsigel@gmail.com>
2026-04-21 02:11:45 -0300

    ejercicio para practicar desarrollo por capas, logica y gui

---
+++ src/ejemploIGU/logica/Logica.java
@@ -0,0 +1,12 @@
+package ejemploIGU.logica;
+
+import ejemploIGU.igu.Principal;
+
+public class Logica {
+
+    public static void main(String[] args) {
+        Principal ventana = new Principal();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+}
