b0765e18648c72f23e6589a61fd4de9321387742
zotel1 <zotelsigel@gmail.com>
2026-05-31 12:09:00 -0300

    asignacion y declaracion de matrices

---
+++ src/matrices/ejercicioUno.java
@@ -0,0 +1,14 @@
+package matrices;
+
+public class ejercicioUno {
+
+    static void main() {
+        int matriz [][] = new int[3][3];
+
+        // Asignacion manual
+
+        matriz[0][0] = 5;
+        matriz[0][1] = 13;
+        matriz[2][2] = 24;
+    }
+}
